c7cdf2d2e1310005360fb174fc68e2f0f87b8e94
B1stmode <tima.gamedev@gmail.com>
2026-08-23 16:33:01 +0200

    Simple DOM projects generation
    Created an array of hard coded projects for now, and a logic that asigns them to the 3 available UI slots, NEXT: randomise the asignment of projects to the UI slots + Add more projects

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const generateButton = document.querySelector('#generateButton');
-const challengeHeader = document.querySelector('#challengeHeader');
-const firstOption = document.querySelector('#challengeList li'); 
+const generateButton = document.querySelector("#generateButton");
+const alloptions = document.querySelectorAll("#challengeList .textPart");
+
+const projectIdeas = [
+  "Calculator",
+  "Weather App",
+  "Pomodoro Timer",
+  "Roll a Dice",
+  "Rock, Paper, Scissors",
+  "Simple CLI tool",
+];
 
 const handleGenerateClick = () => {
-    console.log("Button was clicked");
-    firstOption.textContent = "Calculator Stack: JS, HTML, CSS";
-    
-}
-generateButton.addEventListener('click', handleGenerateClick);
+    console.log("pressed")
+  alloptions.forEach((span, index) => {
+    if (projectIdeas[index]) {
+      span.textContent = projectIdeas[index];
+    }
+  });
+};
+generateButton.addEventListener("click", handleGenerateClick);
