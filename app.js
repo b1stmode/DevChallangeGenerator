df03b304a59d80cccc31047af8f567d96a4ed1de
B1stmode <tima.gamedev@gmail.com>
2026-09-15 20:38:58 +0200

    chalange choosing button functionality pt.2 + better projects data structure

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const alloptions = document.querySelectorAll("#challengeList .textPart");
 
 const allButtons = document.querySelectorAll(".optionButton");
 
+let selectedProject;
+
 const projectIdeas = [
-  "Calculator",
-  "Weather App",
+  {
+    name: "Calculator",
+    description:
+      "A simple calculator app that can perform basic arithmetic operations.",
+    category: "Utility",
+    difficulty: "Easy",
+    tags: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    name: "Weather App",
+    description:
+      "An app that fetches and displays weather information based on user location.",
+    category: "Utility",
+    difficulty: "Medium",
+    tags: ["JavaScript", "API", "HTML", "CSS"],
+  },
   "Pomodoro Timer",
   "Roll a Dice",
   "Rock, Paper, Scissors",
@@ -20,13 +36,11 @@ const handleGenerateClick = () => {
   alloptions.forEach((span) => {
     let randomIndex = Math.floor(Math.random() * 6);
     while (usedIndexes.includes(randomIndex)) {
-        randomIndex = Math.floor(Math.random() * 6);
+      randomIndex = Math.floor(Math.random() * 6);
     }
     const randomProject = projectIdeas[randomIndex];
-    span.textContent = randomProject;
-    usedIndexes.push(randomIndex)
-
-    
+    span.textContent = randomProject.name;
+    usedIndexes.push(randomIndex);
   });
 };
 
@@ -36,8 +50,9 @@ const handleOptionClick = (event) => {
   const button = event.target;
   const chooseOption = button.parentElement.querySelector(".textPart");
 
-  console.log(chooseOption.textContent);
-}
+  selectedProject = chooseOption.textContent;
+  console.log(selectedProject);
+};
 
 allButtons.forEach((button) => {
   button.addEventListener("click", handleOptionClick);
