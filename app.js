ad401bde32f6c5ca5961c7a2a709cd073b55242d
B1stmode <tima.gamedev@gmail.com>
2026-09-25 12:02:41 +0200

    JS formating

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-import { projectIdeas } from "./projects.js";
+  import { projectIdeas } from "./projects.js";
 
-const generateButton = document.querySelector("#generateButton");
-const alloptions = document.querySelectorAll("#challengeList .textPart");
+  const generateButton = document.querySelector("#generateButton");
+  const alloptions = document.querySelectorAll("#challengeList .textPart");
 
-const allButtons = document.querySelectorAll(".optionButton");
+  const allButtons = document.querySelectorAll(".optionButton");
 
-let selectedProject;
+  let selectedProject;
 
-const handleGenerateClick = () => {
-  console.log("pressed");
+  const handleGenerateClick = () => {
+    console.log("pressed");
 
-  const usedIndexes = [];
+    const usedIndexes = [];
 
-  alloptions.forEach((span) => {
-    let randomIndex = Math.floor(Math.random() * 6);
-    while (usedIndexes.includes(randomIndex)) {
-      randomIndex = Math.floor(Math.random() * 6);
-    }
-    const randomProject = projectIdeas[randomIndex];
-    span.textContent = randomProject.name;
-    usedIndexes.push(randomIndex);
+    alloptions.forEach((span) => {
+      let randomIndex = Math.floor(Math.random() * 6);
+      while (usedIndexes.includes(randomIndex)) {
+        randomIndex = Math.floor(Math.random() * 6);
+      }
+      const randomProject = projectIdeas[randomIndex];
+      span.textContent = randomProject.name;
+      usedIndexes.push(randomIndex);
+    });
+  };
+
+  generateButton.addEventListener("click", handleGenerateClick);
+
+  const handleOptionClick = (event) => {
+    const button = event.target;
+    const chooseOption = button.parentElement.querySelector(".textPart");
+
+    selectedProject = chooseOption.name;
+    console.log(selectedProject);
+  };
+
+  allButtons.forEach((button) => {
+    button.addEventListener("click", handleOptionClick);
   });
-};
-
-generateButton.addEventListener("click", handleGenerateClick);
-
-const handleOptionClick = (event) => {
-  const button = event.target;
-  const chooseOption = button.parentElement.querySelector(".textPart");
-
-  selectedProject = chooseOption.textContent;
-  console.log(selectedProject);
-};
-
-allButtons.forEach((button) => {
-  button.addEventListener("click", handleOptionClick);
-});
